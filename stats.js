5d71d49fc3699c9f581432d46a9e125d7cce0b4a
deniscapp <denis_cappelini@hotmail.com>
2016-08-08 23:53:59 -0300

    small changes in stats.js

---
+++ stats.js
@@ -0,0 +1,33 @@
+function sendData(points, partialPoints, win, errors, level, levelId, size, end){
+    var info = {};
+    var path;
+    if(window.location.hostname == "remar.dc.ufscar.br"){
+        path = "/exported-resource/saveStats"
+    }else {
+        path = "http://remar.dc.ufscar.br/exported-resource/saveStats"
+    }
+    $.getJSON("remar.json", function(json) {
+
+        info.exportedResourceId = json.exportedResourceId;
+        info.points = points.toFixed(0);
+        info.partialPoints = partialPoints.toFixed(0);
+        info.errors = errors;
+        info.answer = level.palavra;
+        info.question = level.dica;
+        info.levelId = levelId;
+        info.end = end;
+        info.size = size;
+        info.win = win;
+        info.gameType = 'questionAndAnswer';
+        $.ajax({
+            type: "POST",
+            url: path,
+            data: info,
+            success: function(data) {
+            }
+        })
+    });
+
+
+
+}
