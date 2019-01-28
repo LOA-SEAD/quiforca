9df9b5eb0316f652894a3dc447f7557f158fb427
JhonataQuerobim <jhonataquerobim@gmail.com>
2019-01-28 16:18:47 -0200

    Corrigido bug ao acertar palavra

--- js/logica.js
+++ js/logica.js
@@ -111,7 +111,6 @@ function update()
 			criarCamadaDerrota();
 			break;
 		case 1:
-			//Fim de jogo: jogador ganhou
 			var el = document.getElementById("camadaJogo");
 
 			aux = 5*Math.pow(0.8, jogo.erros);
@@ -121,7 +120,7 @@ function update()
 			$('<div>').attr({'id': 'palavraCerta',})
 				.appendTo(el);
 
-			el.onclick = function() {
+			el.onmousedown = function() {
 				destruirCamadaJogo();
 				criarCamadaVitoria();
 			}
