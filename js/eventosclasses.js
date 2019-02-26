534d136e2cc52963585644c167cc73f2add4f58e
Caio Vinicius <caio.b.santos@hotmail.com>
2019-02-26 14:31:16 -0300

    bugFix - leitura em outros navegadores

--- js/eventosclasses.js
+++ js/eventosclasses.js
@@ -428,12 +428,9 @@ function leituraDica()
 
 function testeLeitura(texto)
 {
-	var voices = window.speechSynthesis.getVoices();
 	var msg = new SpeechSynthesisUtterance(texto);
 	msg.volume = 1; // 0 to 1
 	msg.rate = 1.3; // 0.1 to 10
 	msg.lang = "pt-BR";
-	msg.voiceURI = 'Google português do Brasil';
-	msg.voice = voices[15];
 	window.speechSynthesis.speak(msg);
 }
