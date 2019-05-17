219a9c3e194ba6c7f804672b5e2d407ea9a047f5
Caio Vinicius <caio.b.santos@hotmail.com>
2019-05-17 17:38:09 -0300

    ultima alteracao

--- js/eventosclasses.js
+++ js/eventosclasses.js
@@ -7,10 +7,19 @@ function Palco()
 		//Cria a variavel palco que irá conter todas as camadas
 		var palco = document.createElement("div");
 
+		
+		// We listen to the resize event
+		window.addEventListener('resize', () => {
+			// We execute the same script as before
+			let vh = window.innerHeight * 0.01;
+			document.documentElement.style.setProperty('--vh', `${vh}px`);
+		});
+
 		// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 		let vh = window.innerHeight * 0.01;
 		// Then we set the value in the --vh custom property to the root of the document
 		document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 		//Coloca a id no palco
 		palco.setAttribute("id", "palco");
 		//Coloca palco como filho de body
